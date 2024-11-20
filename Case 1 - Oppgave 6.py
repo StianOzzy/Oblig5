78d5e2253778e71dd992ec2b6b0663df0170f44c
Stozo <35351828+Stozo@users.noreply.github.com>
2024-11-20 21:25:36 +0100

    Pressing buttons (Bip)🔥

---
+++ Case 1 - Oppgave 6.py
@@ -0,0 +1,27 @@
+'''
+
+Case 1 - Oppgave 6
+
+Skriv en funksjon som returnerer prisen (med skatt) av en gitt handlevogn.
+
+Ta fortsatt utgangspunkt i at handlevogn-dictionarien er på følgende format:
+
+{
+
+<varenøkkel 1>: <antall av vare i handlevognen>,
+
+<varenøkkel 2>: <antall av vare i handlevognen>,
+
+etc.
+}
+
+Denne funksjonen skal hete calculate_shopping_cart_price() og skal minst ta tre input-parametere:
+
+shopping_cart - En handlevogn - dictionary
+
+all_wares - Et vareregister - dictionary
+
+tax - En skatteprosent - float (standardverdi for skatteprosent skal være 25%) '
+
+
+'''
