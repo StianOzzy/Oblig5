b87a6c4662752cbef1b2726ff469d42eacb379b0
Stozo <35351828+Stozo@users.noreply.github.com>
2024-11-20 19:41:49 +0100

    ballin

--- Case 1 - Oppgave 1.py
+++ Case 1 - Oppgave 1.py
@@ -11,32 +11,39 @@ Funksjonen skal hete print_ware_information() og skal ta én parameter:
 
 '''
 
+# Oppgi ware i format: (all_wares["amd_processor"])
 def print_ware_information(ware):
-    print(f"Name: {ware.name}")
-    print(f"Price: {ware.price},-")
-    print(f"Number in stock: {ware.stock}")
-    print(f"Description: {ware.description}")
+    print(f"Name: {ware["name"]}")
+    print(f"Price: {ware["price"]},-")
+    print(f"Number in stock: {ware["number_in_stock"]}")
+    print(f"Description: {ware["description"]}")
+
+# -------------------------------------------------------------------------------------------------------------------- #
+
+all_wares = {
+"amd_processor": {
+"name": "AMD Ryzen 9 5900X Processor",
+"price": 5590.0,
+"number_in_stock": 50,
+"ratings": [4.5, 4.0, 5.0, 5.0, 4.5, 3.0],
+"description": "All the cores and threads you'll need!",
+},
+"playstation_5": {
+"name": "PlayStation 5",
+"price": 5999.0,
+"number_in_stock": 0,
+"ratings": [5.0, 5.0, 4.5, 2.0, 5.0, 4.5, 4.0],
+"description": "Next generation console, never in stock!",
+},
+"hdmi_cable": {
+"name": "Belkin Ultra High Speed HDMI Cable - 2m",
+"price": 349.0,
+"number_in_stock": 3,
+"ratings": [5.0, 5.0, 4.5, 5.0, 5.0, 5.0],
+"description": "A high speed overprices HDMI cable!",
+}
+}
 
 
-myProducts = {
-  "child1" : {
-    "name" : "Emil",
-    "price" : 9,
-    "stock" : 30,
-    "description" : "Shitty ass kid"
-  },
-  "child2" : {
-    "name" : "Tobias",
-    "price" : 45,
-    "stock" : 30,
-    "description" : "Decent kid"
-  },
-  "child3" : {
-    "name" : "Linus",
-    "price" : 17,
-    "stock" : 30,
-    "description" : "mid af"
-  }
-} 
-
-print_ware_information(child1)
+ # TEST
+print_ware_information(all_wares["amd_processor"])
--- Case 1 - Oppgave 2.py
+++ Case 1 - Oppgave 2.py
@@ -11,8 +11,38 @@ Funksjonen skal hete calculate_average_ware_rating() og skal ta minst én parame
 
 def calculate_average_ware_rating(ware):
     rating = 0
-    if len(ware.ratings) > 0:
-        for ratings in range(len(ware.rating)):
-            rating += ware.rating[ratings]
-        return rating/len(ware.rating)
+    if len(ware["ratings"]) > 0:
+        for i in range(len(ware["ratings"])):
+            rating += ware["ratings"][i]
+        return round(rating/len(ware["ratings"]),1)
     else: return 0
+
+# -------------------------------------------------------------------------------------------------------------------- #
+
+all_wares = {
+"amd_processor": {
+"name": "AMD Ryzen 9 5900X Processor",
+"price": 5590.0,
+"number_in_stock": 50,
+"ratings": [4.5, 4.0, 5.0, 5.0, 4.5, 3.0],
+"description": "All the cores and threads you'll need!",
+},
+"playstation_5": {
+"name": "PlayStation 5",
+"price": 5999.0,
+"number_in_stock": 0,
+"ratings": [5.0, 5.0, 4.5, 2.0, 5.0, 4.5, 4.0],
+"description": "Next generation console, never in stock!",
+},
+"hdmi_cable": {
+"name": "Belkin Ultra High Speed HDMI Cable - 2m",
+"price": 349.0,
+"number_in_stock": 3,
+"ratings": [5.0, 5.0, 4.5, 5.0, 5.0, 5.0],
+"description": "A high speed overprices HDMI cable!",
+}
+}
+
+
+ # TEST
+print(calculate_average_ware_rating(all_wares["amd_processor"]))
