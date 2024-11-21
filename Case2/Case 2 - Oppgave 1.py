3a112547f5b2b1fa982df5b444d16c22c230655d
Stozo <35351828+Stozo@users.noreply.github.com>
2024-11-21 21:07:27 +0100

    Oppgave 1 done

--- Case2/Case 2 - Oppgave 1.py
+++ Case2/Case 2 - Oppgave 1.py
@@ -1,4 +1,4 @@
-'''
+"""
 
 Implementer funksjonen print_car_information() som printer ut informasjon om en bil på følgende format:
 
@@ -8,5 +8,66 @@ Price. 96000,-
 Manufactured: 2012-8
 Condition: Used
 
-'''
+"""
 
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# Case 2 - Oppgave 1
+
+def print_car_information(car):
+   print(f"Brand: {car["brand"]}")
+   print(f"Model: {car["model"]}")
+   print(f"Price: {car["price"]}")
+   print(f"Manufactured: {car["year"]}-{car["month"]}")
+   if car["new"] == True:
+       print("Condition: New")
+   else:
+       print("Condition: Old")
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# Fra CAR_DEALERSHIP.PY
+
+from datetime import date
+
+car_register = {
+"toyotaBZ4X": {
+"brand": "Toyota",
+"model": "Corolla",
+"price": 96_000,
+"year": 2012,
+"month": 8,
+"new": False,
+"km": 163_000
+},
+"pugeot408": {
+"brand": "Pugeot",
+"model": "408",
+"price": 330_000,
+"year": 2019,
+"month": 1,
+"new": False,
+"km": 40_000
+},
+"audiRS3": {
+"brand": "Audi",
+"model": "RS3",
+"price": 473_000,
+"year": 2022,
+"month": 2,
+"new": True,
+"km": 0
+},
+}
+
+NEW_CAR_REGISTRATION_FEE = 8745
+RENT_CAR_PERCENTAGE = 0.4
+RENT_NEW_CAR__FEE = 1000
+
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# TEST Case 2 - Oppgave 1
+
+toyota = car_register['toyotaBZ4X']
+print_car_information(toyota)
