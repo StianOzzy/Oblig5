2cf4e92de63b79c2c60be432fbe92c78f27e9a21
Stozo <35351828+Stozo@users.noreply.github.com>
2024-11-21 21:42:04 +0100

    added functionality of create_car() and get_car_age()

---
+++ Case2/new_CAR_DEALERSHIP.py
@@ -0,0 +1,97 @@
+from datetime import date
+
+car_register = {
+    "toyotaBZ4X": {
+        "brand": "Toyota",
+        "model": "Corolla",
+        "price": 96_000,
+        "year": 2012,
+        "month": 8,
+        "new": False,
+        "km": 163_000
+    },
+    "pugeot408": {
+        "brand": "Pugeot",
+        "model": "408",
+        "price": 330_000,
+        "year": 2019,
+        "month": 1,
+        "new": False,
+        "km": 40_000
+    },
+    "audiRS3": {
+        "brand": "Audi",
+        "model": "RS3",
+        "price": 473_000,
+        "year": 2022,
+        "month": 2,
+        "new": True,
+        "km": 0
+    },
+}
+
+NEW_CAR_REGISTRATION_FEE = 8745
+RENT_CAR_PERCENTAGE = 0.4
+RENT_NEW_CAR_FEE = 1000
+
+# DONE
+def print_car_information(car):
+   print(f"Brand: {car["brand"]}")
+   print(f"Model: {car["model"]}")
+   print(f"Price: {car["price"]}")
+   print(f"Manufactured: {car["year"]}-{car["month"]}")
+   if car["new"] == True:
+       print("Condition: New")
+   else:
+       print("Condition: Old")
+
+def create_car(car_register,brand, model, price, year, month, new=True, km=0):
+    car_key = brand.lower()+model.capitalize()
+    car_register[car_key] = {
+        "brand": brand,
+        "model": model,
+        "price": price,
+        "year": year,
+        "month": month,
+        "new": new,
+        "km" : km
+    }
+    return car_key
+
+
+def get_car_age(car):
+    # print(date.today().year)
+    age = date.today().year - car["year"]
+    return age
+
+
+def next_eu_control(car):
+    pass
+
+
+def rent_car_monthly_price(car):
+    pass
+
+
+def calculate_total_price(car):
+    pass
+
+
+def is_new(car):
+    return car['new']
+
+
+if __name__ == '__main__':
+    create_car(car_register, "Volvo", "V90", 850_000, 2021, 12, True, 0)
+
+    toyota = car_register['toyotaBZ4X']
+    print_car_information(toyota)
+    print(f"\nThe total price for this {toyota['brand']} {toyota['model']} is {calculate_total_price(toyota)}kr.")
+    print(f"Next EU-control for the {toyota['brand']} {toyota['model']} is {next_eu_control(toyota)}")
+    print(f"If you want to rent the {toyota['brand']} {toyota['model']} the monthly fee will be {rent_car_monthly_price(toyota)}kr.")
+
+    audi = car_register['audiRS3']
+    print_car_information(audi)
+    print(f"\nThe total price for this {audi['brand']} {audi['model']} is {calculate_total_price(audi)}kr.")
+    print(f"Next EU-control for the {audi['brand']} {audi['model']} is {next_eu_control(audi)}")
+    print(f"If you want to rent the {audi['brand']} {audi['model']} the monthly fee will be {rent_car_monthly_price(audi)}kr.")
