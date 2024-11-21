a470b07739b587025dcdf98ad75825e25fabdfdb
Stozo <35351828+Stozo@users.noreply.github.com>
2024-11-21 20:47:40 +0100

    Pressing buttons 🔥

---
+++ Case2/Case 2 - Oppgave 1.py
@@ -0,0 +1,12 @@
+'''
+
+Implementer funksjonen print_car_information() som printer ut informasjon om en bil på følgende format:
+
+Brand: Toyota
+Model: Corolla
+Price. 96000,-
+Manufactured: 2012-8
+Condition: Used
+
+'''
+
